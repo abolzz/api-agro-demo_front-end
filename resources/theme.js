33fa306ac067e1a615e7c46ea5569ef5dc1dd50c
abolzz <abolzzy@gmail.com>
2018-07-16 20:59:14 +0300

    Pievienots sem.lv boilerplate

---
+++ resources/theme.js
@@ -0,0 +1,9 @@
+// Vendor resources (normalize.css, jquery and plugins by your choice)
+require("./vendor/normalize.min.css");
+window.$ = window.jQuery = require("./vendor/jquery-3.3.1.min");
+
+// Your own CSS files
+require("./scss/style.scss");
+
+// Your own javascript files
+require("./js/app.js");
